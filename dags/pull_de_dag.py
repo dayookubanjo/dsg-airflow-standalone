6f1f480f77f95785b19d49e581e9d287661f97f7
likith1996 <121960360+likith1996@users.noreply.github.com>
2023-03-21 13:46:47 -0400

    UPDATED DE dag

--- dags/pull_de_dag.py
+++ dags/pull_de_dag.py
@@ -13,14 +13,21 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
 from airflow.providers.amazon.aws.operators.sns import SnsPublishOperator
-from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor 
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.models import Variable 
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
-    
 
+#-----Importing Variables----
+SNS_ARN=Variable.get("SNS_ARN")
+DATAMART_DATABASE = Variable.get("DATAMART_DATABASE")
+AIML_DATABASE = Variable.get("AIML_DATABASE")
+DE_DATABASE = Variable.get("DE_DATABASE")
+LOAD_CONNECTION = Variable.get("LOAD_CONNECTION")
+TRANSFORM_CONNECTION = Variable.get("TRANSFORM_CONNECTION")
+DE_LAUNCH_TEMPLATE_ID = Variable.get("DE_LAUNCH_TEMPLATE_ID")
+S3_DE_BUCKET=Variable.get("S3_DE_BUCKET")
 # ---- Global variables ----
-SNOWFLAKE_LOAD_CONNECTION = "Airflow-Dev_load-connection"
-SNOWFLAKE_TRANSFORM_CONNECTION = "Airflow-Dev_Transform-connection" 
 EC2_WAITTIME = 3*60
 POKE_INTERVAL = 10
 POKE_TIMEOUT = 25*60
@@ -30,8 +37,8 @@ FILE_SIZE_IN_BYTES = 2.5 * 1000 * 1048576
 def on_failure_callback(context):
     op = SnsPublishOperator(
         task_id="dag_failure"
-        ,target_arn="arn:aws:sns:us-east-2:698085094823:EC2_Notification"
-        ,subject="DAG FAILED"
+        ,target_arn=SNS_ARN
+        ,subject="DIGITAL-ELEMENT DAG FAILED"
         ,message=f"Task has failed, task_instance_key_str: {context['task_instance_key_str']}"
     )
     op.execute(context)
@@ -39,8 +46,8 @@ def on_failure_callback(context):
 def on_timeout_callback(context):
     op = SnsPublishOperator(
         task_id="dag_timeout"
-        ,target_arn="arn:aws:sns:us-east-2:698085094823:EC2_Notification"
-        ,subject="DAG TIMED OUT"
+        ,target_arn=SNS_ARN
+        ,subject="DIGITAL-ELEMENT DAG TIMED OUT"
         ,message=f"Task has timed out as there's no data to process, task_instance_key_str: {context['task_instance_key_str']}"
     )
     op.execute(context)
@@ -50,8 +57,8 @@ def on_timeout_callback(context):
 def on_success_callback(context):
     op = SnsPublishOperator(
         task_id="dag_success"
-        ,target_arn="arn:aws:sns:us-east-2:698085094823:EC2_Notification"
-        ,subject="DAG Success"
+        ,target_arn=SNS_ARN
+        ,subject="DIGITAL-ELEMENT DAG Success"
         ,message=f"Digital Element ingestion DAG has succeeded, run_id: {context['run_id']}"
     )
     op.execute(context)
@@ -66,21 +73,21 @@ def check_fn(files: List) -> bool:
 
 # ---- Query definitions ------
 snowflake_ingestion_query = [
-    """truncate table "DEV_DIGITAL_ELEMENT"."RAW_DATA"."DE_FLAT_FILE";""",
-    """copy into "DEV_DIGITAL_ELEMENT"."RAW_DATA"."DE_FLAT_FILE" from @dev_digital_element.raw_data.dev_digital_element purge=TRUE;"""
+    f"""truncate table {DE_DATABASE}.raw_data.de_flat_file;""",
+    f"""copy into {DE_DATABASE}.raw_data.de_flat_file from @{DE_DATABASE}.raw_data.dev_digital_element purge=TRUE;"""
     ]
 
 data_cleaning_query = [
-    """create or replace table dev_digital_element.mappings.ip_range_mappings_filtered as
+    f"""create or replace table {DE_DATABASE}.mappings.ip_range_mappings_filtered as
 select
     current_date as date_updated,
     ip_range_start,
-    dev_digital_element.public.ip_to_number(ip_range_start) as ip_range_start_numeric,
+    {DE_DATABASE}.public.ip_to_number(ip_range_start) as ip_range_start_numeric,
     ip_range_end,
-    dev_digital_element.public.ip_to_number(ip_range_end) as ip_range_end_numeric,
+    {DE_DATABASE}.public.ip_to_number(ip_range_end) as ip_range_end_numeric,
     trim(upper(pulseplus_country)) as normalized_country_code,
     trim(upper(pulseplus_region)) as normalized_region_code,
-    dev_digital_element.public.normalize_city_de(pulseplus_city) as normalized_city_name,
+    {DE_DATABASE}.public.normalize_city_de(pulseplus_city) as normalized_city_name,
     pulseplus_metro_code as metro_code,
     pulseplus_postal_code as normalized_zip,
     pulseplus_country_conf as country_confidence,
@@ -90,8 +97,8 @@ select
     isp_name,
     homebiz_type,
     company_name,
-    dev_datamart.public.domain_normalizer(domain_name) as normalized_company_domain
-from "DEV_DIGITAL_ELEMENT"."RAW_DATA"."DE_FLAT_FILE"
+    {DATAMART_DATABASE}.public.domain_normalizer(domain_name) as normalized_company_domain
+from {DE_DATABASE}.raw_data.de_flat_file
 where not ((company_name is null or len(company_name)<2) and (domain_name is null or len(domain_name)<2))
 and normalized_country_code in ('USA', 'CAN', 'GBR', 'DEU', 'SWE', 'NLD', 'NOR', 'FRA', 'ESP')
 and not startswith(normalized_company_domain, 'ip-');"""
@@ -120,14 +127,14 @@ with DAG(
     launch_instance_exec = BashOperator(
         task_id = "launch-instance",
         depends_on_past=False,
-        bash_command="aws ec2 run-instances --count 1 --launch-template LaunchTemplateId=lt-016744221119eb24c --region us-east-2"
+        bash_command=f"aws ec2 run-instances --count 1 --launch-template LaunchTemplateId={DE_LAUNCH_TEMPLATE_ID} --region us-east-2"
     )
     
     launch_instance_exec.doc = "This task launches an EC2 instance according to the DE-related launch template. Uses the default version of the launch template."
 
     check_data_ingestion_status = S3KeySensor(
         task_id='check_data_ingestion_status',
-        bucket_name='dev-intent-digital-element',
+        bucket_name=S3_DE_BUCKET,
         bucket_key='flat-files/de_out.txt.gz',
         aws_conn_id='aws_default',
         check_fn=check_fn,
@@ -147,13 +154,13 @@ with DAG(
     snowflake_ingestion_exec = SnowflakeOperator(
         task_id= "load_from_s3",
         sql= snowflake_ingestion_query,
-        snowflake_conn_id= SNOWFLAKE_LOAD_CONNECTION,
+        snowflake_conn_id= LOAD_CONNECTION,
     )
 
     data_cleaning_exec = SnowflakeOperator(
         task_id= "clean_ip_range_mappings",
         sql= data_cleaning_query,
-        snowflake_conn_id= SNOWFLAKE_TRANSFORM_CONNECTION,
+        snowflake_conn_id= TRANSFORM_CONNECTION,
         on_success_callback=on_success_callback
     )
 
